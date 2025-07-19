3b958bcc2dcc3d9eed10e5d4f613f4181cd5b623
Opeyemi <Opeyemi5055@yahoo.com>
2025-07-19 04:41:30 +0100

    post/users

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,8 +1,48 @@
-export interface PillProps{
-  title: string
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
 }
 
-export interface ButtonProps {
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Company {
+  name: string;
+  catchPhrase?: string;
+  bs?: string;
+}
+
+export interface PostProps {
+  userId: number;
+  id: number;
   title: string;
-  styles: string;
+  body: string;
+}
+
+export interface UserProps {
+  id: number;
+  name?: string;
+  username: string;
+  email: string;
+  address?: Address;
+  phone?: string;
+  website?: string;
+  company: Company;
+}
+
+export interface PostData {
+  userId: number;
+  id?: number;
+  body: string;
+  title: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
 }
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,49 @@
-let user = null;
+import UserCard from "@/components/common/UserCard";
+import Header from "@/components/layout/Header";
+import { UserProps } from "@/interfaces";
+import React from "react";
+
+const Users: React.FC<UserProps[]> = ({ users }) => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">User Content</h1>
+          <button
+            type="button"
+            className="bg-blue-700 px-4 rounded-full text-white"
+          >
+            Add User
+          </button>
+        </div>
+        <div className="grid grid-cols-3 gap-2">
+          {users?.map(
+            ({ username, email, company, id }: UserProps, key: number) => (
+              <UserCard
+                username={username}
+                email={email}
+                company={company}
+                id={id}
+                key={key}
+              />
+            )
+          )}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+}
+
+export default Users;
